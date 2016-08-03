a6df149cff10ec56e442835d14d854d6d0214aa3
spondbob <spondbob@eamca.com>
2016-08-03 20:35:03 +1000

    complete the search functionality

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -5,18 +5,30 @@ var MainWrapper = require('./../MainWrapper');
 var PostBox = require('./../post/PostBox');
 var SearchForm = require('./SearchForm');
 
-function Home () {
-  return (
-    <div className="home">
-      <MainWrapper>
-        <h1>Claim GST</h1>
-        <p className='lead'>Write a slogan here</p>
-        <SearchForm />
-      </MainWrapper>
-      <h3>Today's Posts</h3>
-      <PostBox url="http://localhost:3000/posts/today" pollInterval={2000} />
-    </div>
-  )
-}
+var Home = React.createClass({
+  getInitialState:function () {
+    return { url: 'http://localhost:3000/posts/today' }
+  },
+  handleSubmitSearch: function (criteria) {
+    var query = criteria.abn + '/' + criteria.date;
+    this.setState({url: 'http://localhost:3000/posts/search/' + query});
+  },
+  render: function () {
+    return (
+      <div className="home">
+        <MainWrapper>
+          <h1>Claim GST</h1>
+          <p className='lead'>Write a slogan here</p>
+          <SearchForm
+            onSubmitSearch={this.handleSubmitSearch} />
+        </MainWrapper>
+        <h3>Today's Posts</h3>
+        <PostBox
+          url={this.state.url}
+          pollInterval={200} />
+      </div>
+    )
+  }
+});
 
 module.exports = Home;
--- app/components/home/SearchForm.js
+++ app/components/home/SearchForm.js
@@ -1,23 +1,59 @@
 var React = require('react')
+var PropTypes = React.PropTypes;
 
-function SearchForm() {
-  return (
-     <div className="row">
+var SearchForm = React.createClass({
+  getInitialState: function() {
+    return {abn: '', date: ''};
+  },
+  handleAbnChange: function(e) {
+    this.setState({abn: e.target.value});
+  },
+  handleDateChange: function(e) {
+    this.setState({date: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var abn = this.state.abn.trim();
+    var date = this.state.date.trim();
+    if (!date || !abn) {
+      return;
+    }
+    this.props.onSubmitSearch({abn: abn, date: date});
+    this.setState({abn: '', date: ''});
+  },
+  render: function () {
+    return (
+      <div className="row">
         <div className="col-lg-8 col-lg-offset-2">
-          <form className="form-inline">
+          <form className="form-inline" onSubmit={this.handleSubmit}>
             <div className="form-group">
               <div className="input-group">
-                <input type="text" className="form-control" placeholder="Input ABN" />
+                <input
+                  type="text"
+                  value={this.state.abn}
+                  onChange={this.handleAbnChange}
+                  className="form-control"
+                  placeholder="Input ABN" />
               </div>
               <div className="input-group">
-                <input type="date" className="form-control" placeholder="date" />
+                <input
+                  type="date"
+                  value={this.state.date}
+                  onChange={this.handleDateChange}
+                  className="form-control"
+                  placeholder="date" />
               </div>
             </div>
-            <button className="btn btn-default" type="button">Search</button>
+            <button className="btn btn-default" type="button" type="submit">Search</button>
           </form>
         </div>
       </div>
     )
+  }
+});
+
+SearchForm.propTypes = {
+  onSubmitSearch: PropTypes.func.isRequired
 }
 
 module.exports = SearchForm;
